7211bcbd537f9349173451173431d3f1dcd0a11c
ValterSj <Valter.sjo@gmail.com>
2022-03-09 16:29:46 +0100

    Fixed highscore so name could be displayed aswell

--- highscore/highscore.js
+++ highscore/highscore.js
@@ -1,30 +1,16 @@
-let array = [{points: 2, name: "Valter"}, {points: 6, name: "Valters"}, {points: 3, name: "Valter3"}, {points: 3, name: "Valter5"}]
+let array = [{points: 2, name: "Valter"}, {points: 6, name: "Valters"}, {points: 3, name: "Valter3"}, {points: 3, name: "Valter5"}, {points: 10, name: "V"} ]
 let pointArray = []
 function testArray(){
-    for(element of array){
-        pointArray.push(element.points)
-       // console.log(element)
-    }
-   // console.log(pointArray)
-    pointArray.sort(function(a, b){return b - a});
-    /*for(var i = 0; i < pointArray.length; i++){
-        console.log(pointArray[i])
-        for(element of array){
-            console.log(element.points)
-            if(pointArray[i] === element.points){
-                console.log(element.name)
-            }
-            
-        }
-        
-    }
-    */
+    
+   //sorts the array in decending order
+   array.sort(function(a, b){return b.points - a.points});
+    
    let html = ``;
-   for (let index = 0; index < pointArray.length; index++) {
-       const element = pointArray[index];
+   for (let index = 0; index < array.length; index++) {
+       const element = array[index];
        html += 
        `
-       <p><span style="font-weight:bold">${index+1}.</span> ${element}</p>
+       <p><span style="font-weight:bold">${index+1}.</span> ${element.points} - ${element.name}</p>
        `
        
    }
